7c280952ed24b691272641b1ec0f904fe44ac1f7
crowden <christopherrowden@gmail.com>
2017-10-31 12:58:57 -0500

    ...refactoring

--- js/md_help/md-help.js
+++ js/md_help/md-help.js
@@ -10,25 +10,25 @@ export default class MdPopupHelper extends React.Component {
         const md_helper_linebreaks = (
           <Popover id="popover-trigger-click-root-close" title="Line Breaks">
               <p className="md-help-popover-explanation">Before hitting return, end a line with two spaces to add a {br} linebreak.</p>
-              <p className="md-help-popover-example">How do I love thee?<span className="spaces">{'\xa0\xa0'}</span><br/>Let me count the ways.</p>
+              <p className="md-help-popover-example">How do I love thee?<span className="highlight">{'\xa0\xa0'}</span><br/>Let me count the ways.</p>
           </Popover>
         );
         const md_helper_bold_italics = (
           <Popover id="popover-trigger-click-root-close" title="Bold and Italics">
               <p className="md-help-popover-explanation">To make something italics or bold, use asterisks ( * ) and underscores ( _ ).</p>
-              <p className="md-help-popover-example">*This is italicized*, and so is _this_.<br/><br/>**This is bold**, and so is __this__.<br/><br/>Use ***italics and bold together*** if you ___have to___.</p>
+              <p className="md-help-popover-example"><span className="highlight">*</span>This is italicized<span className="highlight">*</span>, and so is <span className="highlight">_</span>this<span className="highlight">_</span>.<br/><span className="highlight">**</span>This is bold<span className="highlight">**</span>, and so is <span className="highlight">__</span>this<span className="highlight">__</span>.<br/>Use <span className="highlight">***</span>italics and bold together<span className="highlight">***</span> if you <span className="highlight">___</span>have to<span className="highlight">___</span>.</p>
           </Popover>
         );
         const md_helper_links = (
           <Popover id="popover-trigger-click-root-close" title="Links">
               <p className="md-help-popover-explanation">A link is composed of text that is linked, the destination of the link, and text that appears when the user hovers over the link.</p>
-              <p className="md-help-popover-example">Here is a link to [Google](http://www.google.com/ "go to Google's homepage!"). Based on the explanation above, a link is [the link text](http://www.destination "hover text")</p>
+              <p className="md-help-popover-example">Here is a link to <span className="highlight">[Google](http://www.google.com/ "go to Google's homepage!")</span>. Based on the explanation above, a link is <span className="highlight">[the link text](http://www.destination "hover text")</span></p>
           </Popover>
         );
         const md_helper_subheadings = (
           <Popover id="popover-trigger-click-root-close" title="Subheadings">
-              <p className="md-help-popover-explanation">Subheadins break up content into different sections for your readers. Usually, you should only be adding subheadings 2-3 levels deep. Heading 1's are reserved for page titles and will be removed if added.</p>
-              <p className="md-help-popover-example">## Heading 2<br />### Heading 3<br />#### Heading 4</p>
+              <p className="md-help-popover-explanation">Subheadings break up content into different sections for your readers. Usually, you should only be adding subheadings 2-3 levels deep. Heading 1's are reserved for page titles and will be removed if added.</p>
+              <p className="md-help-popover-example"><span className="highlight">## </span>Heading 2<br /><span className="highlight">### </span>Heading 3<br /><span className="highlight">#### </span>Heading 4</p>
           </Popover>
         );
         const md_helper_horizontal_rules = (
@@ -42,14 +42,14 @@ export default class MdPopupHelper extends React.Component {
               <p className="md-help-popover-explanation">Lists can be ordered (1., 2., 3., etc.) or unordered with bullets.</p>
               <p className="md-help-popover-example">
                 A bulleted list:
-                <br />- Use a hyphen for each bulleted list item
-                <br />- Use a hyphen for each bulleted list item
-                <br />- Use a hyphen for each bulleted list item
+                <br /><span className="highlight">- </span>Use a hyphen for each bulleted list item
+                <br /><span className="highlight">- </span>Use a hyphen for each bulleted list item
+                <br /><span className="highlight">- </span>Use a hyphen for each bulleted list item
                 <br /><br />
                 An ordered list:
-                <br />1. Numbered lists are easy
-                <br />2. Markdown keeps track of the numbers for you
-                <br />7. So this will be item 3.
+                <br /><span className="highlight">1. </span>Numbered lists are easy
+                <br /><span className="highlight">2. </span>Markdown keeps track of the numbers for you
+                <br /><span className="highlight">7. </span>So this will be item 3.
               </p>
           </Popover>
         );
@@ -57,10 +57,10 @@ export default class MdPopupHelper extends React.Component {
           <Popover id="popover-trigger-click-root-close" title="Nested Lists">
               <p className="md-help-popover-explanation">Nested list are created by placing lists (ordered or unordered) within other lists. Simply indent nested list items with 4 spaces.</p>
               <p className="md-help-popover-example">
-                1. Lists in a list item:<br />
-                <span className="spaces">{'\xa0\xa0\xa0\xa0'}</span>- Indented four spaces.<br />
-                <span className="spaces">{'\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0'}</span>- indented eight spaces.<br />
-                <span className="spaces">{'\xa0\xa0\xa0\xa0'}</span>- Indented four spaces again.
+                <span className="highlight">1. </span>Lists in a list item:<br />
+                <span className="highlight">{'\xa0\xa0\xa0\xa0'}</span>- Indented four spaces.<br />
+                <span className="highlight">{'\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0'}</span>- indented eight spaces.<br />
+                <span className="highlight">{'\xa0\xa0\xa0\xa0'}</span>- Indented four spaces again.
               </p>
           </Popover>
         );
