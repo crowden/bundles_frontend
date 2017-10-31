2630c21836c87cbacb81b4800ed2f255d54ff0cd
crowden <christopherrowden@gmail.com>
2017-10-31 00:43:59 -0500

    ...refactoring

--- js/md_help/md-help.js
+++ js/md_help/md-help.js
@@ -1,17 +1,108 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Popover, OverlayTrigger, Button} from 'react-bootstrap';
+import {OverlayTrigger, Popover, Button} from 'react-bootstrap';
 
-const md_helper_linebreaks = (
-  <Popover id="popover-trigger-click-root-close" title="Line Breaks">
-    This is a line break:
-  </Popover>
-);
+export default class MdPopupHelper extends React.Component {
+    render(){
+        let br = '(<br/>)';
+        let hr = '(<hr/>)';
 
-ReactDOM.render((
-<OverlayTrigger trigger="click" rootClose placement="right" overlay={md_helper_linebreaks}>
-  <Button>Click w/rootClose</Button>
-</OverlayTrigger>
-), document.queryGetElementById('md-syntax-helper-linebreaks'));
-
-export {md_helper_linebreaks}
+        const md_helper_linebreaks = (
+          <Popover id="popover-trigger-click-root-close" title="Line Breaks">
+              <p className="md-help-popover-explanation">Before hitting return, end a line with two spaces to add a {br} linebreak.</p>
+              <p className="md-help-popover-example">How do I love thee?<span className="spaces">{'\xa0\xa0'}</span><br/>Let me count the ways.</p>
+          </Popover>
+        );
+        const md_helper_bold_italics = (
+          <Popover id="popover-trigger-click-root-close" title="Bold and Italics">
+              <p className="md-help-popover-explanation">To make something italics or bold, use asterisks ( * ) and underscores ( _ ).</p>
+              <p className="md-help-popover-example">*This is italicized*, and so is _this_.<br/><br/>**This is bold**, and so is __this__.<br/><br/>Use ***italics and bold together*** if you ___have to___.</p>
+          </Popover>
+        );
+        const md_helper_links = (
+          <Popover id="popover-trigger-click-root-close" title="Links">
+              <p className="md-help-popover-explanation">A link is composed of text that is linked, the destination of the link, and text that appears when the user hovers over the link.</p>
+              <p className="md-help-popover-example">Here is a link to [Google](http://www.google.com/ "go to Google's homepage!"). Based on the explanation above, a link is [the link text](http://www.destination "hover text")</p>
+          </Popover>
+        );
+        const md_helper_subheadings = (
+          <Popover id="popover-trigger-click-root-close" title="Subheadings">
+              <p className="md-help-popover-explanation">Subheadins break up content into different sections for your readers. Usually, you should only be adding subheadings 2-3 levels deep. Heading 1's are reserved for page titles and will be removed if added.</p>
+              <p className="md-help-popover-example">## Heading 2<br />### Heading 3<br />#### Heading 4</p>
+          </Popover>
+        );
+        const md_helper_horizontal_rules = (
+          <Popover id="popover-trigger-click-root-close" title="Horizontal Rules">
+              <p className="md-help-popover-explanation">Horizontal rules are simply lines that span across the page and break up content.</p>
+              <p className="md-help-popover-example">Insert a horizontal rule {hr} by putting three or more hyphens on a line by themselves.</p>
+          </Popover>
+        );
+        const md_helper_lists = (
+          <Popover id="popover-trigger-click-root-close" title="Lists">
+              <p className="md-help-popover-explanation">Lists can be ordered (1., 2., 3., etc.) or unordered with bullets.</p>
+              <p className="md-help-popover-example">
+                A bulleted list:
+                <br />- Use a hyphen for each bulleted list item
+                <br />- Use a hyphen for each bulleted list item
+                <br />- Use a hyphen for each bulleted list item
+                <br /><br />
+                An ordered list:
+                <br />1. Numbered lists are easy
+                <br />2. Markdown keeps track of the numbers for you
+                <br />7. So this will be item 3.
+              </p>
+          </Popover>
+        );
+        const md_helper_nested_lists = (
+          <Popover id="popover-trigger-click-root-close" title="Nested Lists">
+              <p className="md-help-popover-explanation">Nested list are created by placing lists (ordered or unordered) within other lists. Simply indent nested list items with 4 spaces.</p>
+              <p className="md-help-popover-example">
+                1. Lists in a list item:<br />
+                <span className="spaces">{'\xa0\xa0\xa0\xa0'}</span>- Indented four spaces.<br />
+                <span className="spaces">{'\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0'}</span>- indented eight spaces.<br />
+                <span className="spaces">{'\xa0\xa0\xa0\xa0'}</span>- Indented four spaces again.
+              </p>
+          </Popover>
+        );
+        
+        return (
+            <div className="popovers-container">
+                <div className="single-popover">
+                    <OverlayTrigger trigger="click" rootClose placement="right" overlay={md_helper_linebreaks}>
+                      <Button>Linebreaks</Button>
+                    </OverlayTrigger>
+                </div>
+                <div className="single-popover">
+                    <OverlayTrigger trigger="click" rootClose placement="right" overlay={md_helper_bold_italics}>
+                      <Button>Italics & Bold</Button>
+                    </OverlayTrigger>
+                </div>
+                <div className="single-popover">
+                    <OverlayTrigger trigger="click" rootClose placement="right" overlay={md_helper_links}>
+                      <Button>Hyperlinks</Button>
+                    </OverlayTrigger>
+                </div>
+                <div className="single-popover">
+                    <OverlayTrigger trigger="click" rootClose placement="right" overlay={md_helper_subheadings}>
+                      <Button>Subheadings</Button>
+                    </OverlayTrigger>
+                </div>
+                <div className="single-popover">
+                    <OverlayTrigger trigger="click" rootClose placement="right" overlay={md_helper_horizontal_rules}>
+                      <Button>Horizontal Rules</Button>
+                    </OverlayTrigger>
+                </div>
+                <div className="single-popover">
+                    <OverlayTrigger trigger="click" rootClose placement="right" overlay={md_helper_lists}>
+                      <Button>Lists</Button>
+                    </OverlayTrigger>
+                </div>
+                <div className="single-popover">
+                    <OverlayTrigger trigger="click" rootClose placement="right" overlay={md_helper_nested_lists}>
+                      <Button>Nested Lists</Button>
+                    </OverlayTrigger>
+                </div>
+            </div>
+        );
+    }
+}
