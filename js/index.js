3292aceabb8b5463fb79776025fd738322bd032a
crowden <christopherrowden@gmail.com>
2017-12-29 16:28:56 -0600

    ...refactoring frontend bundle

--- js/index.js
+++ js/index.js
@@ -4,8 +4,8 @@ export {CrudIndexSorter};
 import MdPopupHelper from './md_help/md-help';
 export {MdPopupHelper};
 
-import {jUtility} from './utility/utility';
-export {jUtility};
+import {jUtility, setEnvironment} from './utility/utility';
+export {jUtility, setEnvironment};
 
 import ToggleHiddenContainer from './utility/toggle-hidden-containers';
 export {ToggleHiddenContainer};
